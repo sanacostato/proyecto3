1142aa104397d466d5e00b8f0025164e22d7bc26
sanacostato <saacosta.dwfsc9@ucamp.utel.edu.mx>
2022-10-08 22:59:48 -0500

    new chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,37 +13,32 @@ function loadData(){
 
         const myChart = new Chart (ctx, {
           type: 'line',
-          data: {
-              labels: labels_for_chart,
-              datasets: [{
-                  label: 'nota',
-                  data: data_for_chart,
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: false
-                  }
-              }
-          }
+  data: data = {
+    labels: labels_for_chart,
+    datasets: [{
+      label: 'Notas',
+      data: data_for_chart,
+      fill: false,
+      borderColor: 'rgba(0, 255, 0, 100)',
+    }]
+  },
+  options: {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: true
+      }
+    },
+    scales: {
+      y: { // defining min and max so hiding the dataset does not change scale range
+        min: 3,
+        max: 10
+      }
+    }
+  }
         });
 
         tblDatos.innerHTML="";
